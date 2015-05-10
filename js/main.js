87701712b3f16c597fc393f9a68664f452f98616
dis-zephyro <zephyro@yandex.ru>
2015-05-11 01:25:52 +0300

    v1

---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+$('.slide').slick({
+    autoplay: true,
+    prevArrow : '<span class="slider-nav prev"><i class="fa fa-chevron-left"></i></span>',
+    nextArrow: '<span class="slider-nav next"><i class="fa fa-chevron-right"></i></span>'
+});
